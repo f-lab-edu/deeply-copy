e6ea7b96491a27bc191043a9f1d4d500f99c25ec
kleekich21 <kangsik.kevin.lee+1@gmail.com>
2024-06-19 16:27:39 +0900

    test: setup jest with ts-jest instead of babel

---
+++ index.test.ts
@@ -0,0 +1,6 @@
+import { describe, expect, jest, test } from "@jest/globals";
+import { deeplyCopy } from "./index";
+
+describe("deeplyCopy", () => {
+  test("primitive Type을 복사한다. ", () => {});
+});
