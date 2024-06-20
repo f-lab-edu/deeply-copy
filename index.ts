bba337c4b4f5bc22c90e665c1a20629d8e7db22a
kleekich21 <kangsik.kevin.lee+1@gmail.com>
2024-06-20 11:10:52 +0900

    feat: handles self reference case with WeakMap, and used generic type for input and output

--- index.ts
+++ index.ts
@@ -1,8 +1,12 @@
-export function deeplyCopy(target: any): any {
+export function deeplyCopy<P>(target: P, hash = new WeakMap()): P {
   if (typeof target === "object" && target !== null) {
+    if (hash.has(target)) {
+      return hash.get(target);
+    }
     const obj: any = Array.isArray(target) ? [] : {};
+    hash.set(target, obj);
     for (let prop in target) {
-      obj[prop] = deeplyCopy(target[prop]);
+      obj[prop] = deeplyCopy(target[prop], hash);
     }
     return obj;
   } else {
