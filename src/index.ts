bd933d21b93757525cb852b2a6ba0a94615e358c
kleekich21 <kangsik.kevin.lee+1@gmail.com>
2024-06-20 10:21:31 +0900

    feat: implement deeplyCopy without self reference case

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
-/* eslint-disable */ // TODO: will be deleted in next PR
-export function deeplyCopy(target: any): any {}
+export function deeplyCopy(target: any): any {
+  if (typeof target === "object" && target !== null) {
+    const obj: any = Array.isArray(target) ? [] : {};
+    for (let prop in target) {
+      obj[prop] = deeplyCopy(target[prop]);
+    }
+    return obj;
+  } else {
+    return target;
+  }
+}
