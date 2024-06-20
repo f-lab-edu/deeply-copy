aaf94d5f52aefaf264e3d1e15819e77954c1d22f
kleekich21 <kangsik.kevin.lee+1@gmail.com>
2024-06-20 11:11:39 +0900

    chore: add JSDoc of deeplyCopy for explanation

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
+/**
+ * 인자로 받은 객체(모든 데이터 타입 포함)를 깊은 복사를 해 불변 객체로 반환한다. 순환 참조를 처리한다.
+ *
+ * @template P
+ * @param {P} target - 복사를 하려는 타겟 객체.
+ * @param {WeakMap<Object, Object>} [hash=new WeakMap()] - 순환 참조를 핸들링하기 위해 WeakMap이 사용되었다.
+ * @returns {P} target을 깊은 복사로 생성한 불변 객체
+ */
 export function deeplyCopy<P>(target: P, hash = new WeakMap()): P {
   if (typeof target === "object" && target !== null) {
     if (hash.has(target)) {
