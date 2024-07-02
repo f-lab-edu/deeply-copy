a8e836b29aade8b15ed0a854256853073c9025a9
kleekich21 <kangsik.kevin.lee+1@gmail.com>
2024-07-02 21:37:11 +0900

    chore: implement renameFiles as typescript, and update script accordingly

---
+++ build/cjs/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deeplyCopy = void 0;
+function deeplyCopy(target) { }
+exports.deeplyCopy = deeplyCopy;
+//# sourceMappingURL=index.js.map
