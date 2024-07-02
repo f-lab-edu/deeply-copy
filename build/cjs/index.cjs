1a30259bc0afb416566b13e46f4639f597ddfe37
kleekich21 <kangsik.kevin.lee+1@gmail.com>
2024-07-02 22:11:02 +0900

    chore: chore: use ts-node as loader to run renameFiles.ts
    NOTE: npx ts-node renameFiles.ts not working
    issue: https://github.com/TypeStrong/ts-node/issues/2100

--- build/cjs/index.cjs
+++ build/cjs/index.cjs
@@ -1,6 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.deeplyCopy = void 0;
-function deeplyCopy(target) { }
 exports.deeplyCopy = deeplyCopy;
+function deeplyCopy(target) { }
 //# sourceMappingURL=index.js.map
--- build/cjs/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.deeplyCopy = void 0;
-function deeplyCopy(target) { }
-exports.deeplyCopy = deeplyCopy;
-//# sourceMappingURL=index.js.map
